7dc91feb0fb8557d91b2943b3a7b4b767b701ae7
Jorge Robles <j.robles@docline.com>
2023-08-07 09:00:21 +0200

    wip

--- public/index.php
+++ public/index.php
@@ -1,19 +1,8 @@
 <?php
 use App\Kernel;
 
-$_SERVER['APP_RUNTIME'] = 'Runtime\\Swoole\\Runtime';
 
-$_SERVER['APP_RUNTIME_OPTIONS'] = [
-    'host' => 'localhost',
-    'port' => 9501,
-    'mode' => SWOOLE_PROCESS,
-    'settings' => [
-        'worker_num' => 4,      // The number of worker processes to start
-        'task_worker_num' => 4,  // The amount of task workers to start
-        'backlog' => 128,       // TCP backlog connection number
-        'document_root'=> dirname(__FILE__)
-    ],
-];
+$_SERVER['APP_RUNTIME_OPTIONS']['settings']['document_root'] = dirname(__FILE__);
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,30 @@
 
 namespace App;
 
+use Runtime\Swoole\ServerAwareInterface;
+use Swoole\Http\Server;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements ServerAwareInterface
 {
     use MicroKernelTrait;
+
+    private Server $server;
+
+    public function setServer(Server $server): void
+    {
+        $this->server = $server;
+    }
+
+    public function getServer(): Server
+    {
+        return $this->server;
+    }
+
+    public function process(ContainerBuilder $container): void
+    {
+
+    }
 }
